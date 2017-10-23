14aa0bf0f593f88f27b7b2735333f2430d27c686
hiRainn <wr_sorarara@yahoo.co.jp>
2017-10-23 22:44:30 +0800

    更改规则默认可以识别中文

--- tags/tags.config.js
+++ tags/tags.config.js
@@ -13,13 +13,13 @@
 		'ajax-type' : 'GET',                //xhr请求方式
 		'li-class-name': 'tag-on',          //li类名称，绑定事件所用
 		'tag-span-class-name': 'tag-items', //放标签的span类名，绑定事件所用
-		'strict-fillter': true,             //严格过滤模式，开启则禁止规则符号输入
+		'strict-fillter': false,             //严格过滤模式，开启则禁止规则符号输入
 		'fillter' : true,                   //正则过滤模式
 		'span-border-color': '#98FB98',     //标签边框颜色
 		'span-color':'#FFF',                //标签颜色
 		'span-mouse-over-color' : '#98FB98',//标签鼠标选中时的颜色
 		'tag-font':'sans-serif',            //标签字体
-		'allow-rule': /^[\w\.]{1,20}$/,      //正则过滤规则
+		'allow-rule': /^[\w\.\u4e00-\u9fa5]{1,20}$/,      //正则过滤规则
 		'error-msg': 'dis-allow-rule',      //过滤模式错误信息
 		'dis-allow-char': ['.','Process','+','=','(',')','*','&','%',' ','，','。',],  //禁止输入字符(字符，不是汉字)
 	}
