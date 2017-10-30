ea4cd09c6b512df4df2a691d8090ff950d525a29
wangrun <wangrun001@microking.cn>
2017-10-30 09:15:55 +0800

    修正

--- pmd/application/index/controller/MyConst.php
+++ pmd/application/index/controller/MyConst.php
@@ -4,5 +4,5 @@ namespace app\index\controller;
 class MyConst
 {
 	const SESSION = 'db_config'; //session_key
-	const LOG_TIME = 10;         //log_time
+	const LOG_TIME = 3600;         //log_time
 }
