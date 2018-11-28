92fb93614066d38d16c9cdc4ce5f211d1b71c383
hiRainn <wr_sorarara@yahoo.co.jp>
2018-11-28 10:11:08 +0800

    Create simple_curl.php

---
+++ simple_curl/simple_curl.php
@@ -0,0 +1,97 @@
+<?php
+
+
+class BaseController 
+{
+
+    /**
+     * 发送HTTP请求方法，目前只支持CURL发送请求 json
+     * @param  string $url 请求URL
+     * @param  array|string $param GET参数数组
+     * @param  array|string $data POST的数据，GET请求时该参数无效
+     * @param  string $method 请求方法GET/POST
+     * @return array|string          响应数据
+     * @throws \Exception
+     */
+    protected function http($url, $param, $data = '', $method = 'GET'){
+        $opts = array(
+            CURLOPT_TIMEOUT        => 30,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+        );
+        /* 根据请求类型设置特定参数 */
+        if($param){
+            $opts[CURLOPT_URL] = $url . '?' . http_build_query($param);
+        }else{
+            $opts[CURLOPT_URL] = $url;
+        }
+        if(strtoupper($method) == 'POST'){
+            $opts[CURLOPT_POST] = 1;
+            $opts[CURLOPT_POSTFIELDS] = $data;
+
+            if(is_string($data)){ //发送JSON数据
+                $opts[CURLOPT_HTTPHEADER] = array(
+                    'Content-Type: application/json; charset=utf-8',
+                    'Content-Length: ' . strlen($data),
+                );
+            }
+        }
+        /* 初始化并执行curl请求 */
+        $ch = curl_init();
+        curl_setopt_array($ch, $opts);
+        $data  = curl_exec($ch);
+        $error = curl_error($ch);
+        curl_close($ch);
+        //发生错误，抛出异常
+        if($error) {
+            throw new \Exception('系统出错',1);
+        }
+        return  $data;
+    }
+    
+    /**
+    * 访问https方法
+    */
+    protected function curl_post_ssl($url, $vars, $second=30,$aHeader=array()) {
+        $ch = curl_init();
+        //超时时间
+        curl_setopt($ch,CURLOPT_TIMEOUT,$second);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
+        //这里设置代理，如果有的话
+        //curl_setopt($ch,CURLOPT_PROXY, '10.206.30.98');
+        //curl_setopt($ch,CURLOPT_PROXYPORT, 8080);
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,false);
+        //以下两种方式需选择一种
+        //第一种方法，cert 与 key 分别属于两个.pem文件
+        //默认格式为PEM，可以注释
+        curl_setopt($ch,CURLOPT_SSLCERTTYPE,'PEM');
+        curl_setopt($ch,CURLOPT_SSLCERT,getcwd().'/Public/crt/apiclient_cert.pem');
+        //默认格式为PEM，可以注释
+        curl_setopt($ch,CURLOPT_SSLKEYTYPE,'PEM');
+        curl_setopt($ch,CURLOPT_SSLKEY,getcwd().'/Public/crt/apiclient_key.pem');
+        //第二种方式，两个文件合成一个.pem文件
+        //curl_setopt($ch,CURLOPT_SSLCERT,getcwd().'/all.pem');
+        
+        if( count($aHeader) >= 1 ){
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $aHeader);
+        }
+
+        curl_setopt($ch,CURLOPT_POST, 1);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$vars);
+        $data = curl_exec($ch);
+        if($data){
+            curl_close($ch);
+            return $data;
+        }
+        else { 
+            $error = curl_errno($ch);
+            echo "call faild, errorCode:$error\n"; 
+            curl_close($ch);
+            return false;
+        }
+    }   
+
+}
